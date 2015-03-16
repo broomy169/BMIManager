bf5bf9eee8ac1f02693979f1991958a45f3ca0cf
jc258876 <graeme.broomhall@my.jcu.edu.au>
2015-03-16 16:30:44 +1000

    final commit

---
+++ src/Patient.java
@@ -0,0 +1,35 @@
+import javax.lang.model.element.Name;
+
+/**
+ * Created by jc258876 on 16/03/15.
+ */
+public class Patient {
+    private String name;
+    private int age;
+    private double height, weight;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setDetails(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getBMI() {
+        return this.weight / (height*height);
+    }
+}
